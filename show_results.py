54c7e3f560ea64496970121c818ca369ca9c2997
O-Sven <1860830@uni-wuppertal.de>
2021-04-12 17:26:14 +0200

    hide all solutions from every exercise

--- show_results.py
+++ show_results.py
@@ -10,17 +10,16 @@ from glob import glob
 # Collect a list of all notebooks in the content folder
 
 hide = False
-if sys.argv[1] == 'hide':
+if len(sys.argv) >1 and sys.argv[1] == 'hide':
     hide = True
-path = ".book/content/Exercises/"
+path = "./book/content/Exercises/"
 print("The path can further be specified. Finish with Return.")
 while True:
-    folder = None
     folder = input("Change all notebooks in " + path)
-    if folder is None:
+    if folder is '':
         break
     path += folder + "/"
-notebooks = glob(folder + "**/*.ipynb", recursive=True)
+notebooks = glob(path + "**/*.ipynb", recursive=True)
 
 # Search through each notebook and look for the loesung tag
 for ipath in notebooks:
@@ -29,11 +28,12 @@ for ipath in notebooks:
     for cell in ntbk.cells:
         cell_tags = cell.get('metadata', {}).get('tags', [])
         if 'loesung' in cell_tags:
-            if hide:
-                cell_tags.append('remove-cell')
-            else:
-                try:
-                    cell_tags.remove('remove-cell')
+            if hide and 'remove_cell' not in cell_tags:
+                cell_tags.append('remove_cell')
+            elif not hide:
+                if 'remove_cell' in cell_tags:
+                    cell_tags.remove('remove_cell')
+            print(cell_tags)
         if len(cell_tags) > 0:
             cell['metadata']['tags'] = cell_tags
 
